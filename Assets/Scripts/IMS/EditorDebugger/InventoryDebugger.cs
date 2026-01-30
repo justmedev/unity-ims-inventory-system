9c02b9b76aae3f6acf74b252bc724d572bc6c223
Ilja Busch <ilja.ragnar@gmail.com>
2026-01-30 10:57:33 +0100

    fix: ims did not build because it used UnityEditor elements

--- Assets/Scripts/IMS/EditorDebugger/InventoryDebugger.cs
+++ Assets/Scripts/IMS/EditorDebugger/InventoryDebugger.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
@@ -267,3 +268,4 @@ namespace IMS.EditorDebugger
         }
     }
 }
+#endif
